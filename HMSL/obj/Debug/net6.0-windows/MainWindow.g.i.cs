0e53270044cbd2256a2702e6138cbe6a53ce7ebc
M0doku <danlll@bk.ru>
2022-06-24 04:15:05 +0500

    HMSL Files Update

--- HMSL/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ HMSL/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -166,7 +166,7 @@ namespace HMSL {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/HMSL;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/HMSL;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
